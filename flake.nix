71ff228cc3634e1ce62f126a693a43092b41cc45
Marc-Antoine Bouchard M <marcantoine.bouchardm@gmail.com>
2025-11-30 19:36:34 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -1,18 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgsSpecific.url = "github:nixos/nixpkgs?ref=e6f23dc08d3624daab7094b701aa3954923c6bbb";
+    bun2nix.url = "github:nix-community/bun2nix";
   };
-  outputs = { self, nixpkgs, nixpkgsSpecific }: let 
+  outputs = { self, nixpkgs, bun2nix }: let 
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     packages.x86_64-linux.hello = pkgs.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
+    packages.x86_64-linux.demo = pkgs.stdenv.mkDerivation {
+      pname = "demo";
+      name = "demo";
+      src = ./.;
+      nativeBuildInputs = [
+        bun2nix.packages.x86_64-linux.default.hook
+      ];
+      bunDeps = bun2nix.packages.x86_64-linux.default.fetchBunDeps {
+        bunNix = ./bun.nix;
+      };
+      buildPhase = ''
+        bun build packages/link/index.ts --outdir packages/link/dist
+        bun build packages/app/index.ts --outfile packages/app/bin/cli --compile
+      '';
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -R packages/app/bin/cli $out
+      '';
+    };
+
     devShells.x86_64-linux.default = pkgs.mkShell {
       nativeBuildInputs = [
-        nixpkgsSpecific.legacyPackages.x86_64-linux.bun
+        pkgs.bun
+        bun2nix.packages.x86_64-linux.default
         pkgs.biome
       ];
     };
